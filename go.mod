92b96ebf1302d07d71ac1bd2a5d6a7aca0172f4c
mohammed <mohammedfuta@gmail.com>
2024-03-10 11:11:17 +0000

    hashedletters and encrypt functions complete

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mohammedfuta2000/caesar-cipher
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const originalLetters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func hashLetters(key int, letters string) (result string) {
+	runes:=[]rune(letters)
+
+	fh:=string(runes[:len(runes)-key])
+	sh:=string(runes[len(runes)-key:])
+	// result = append(result, sh...)
+	// result = sh+fh
+	result = fmt.Sprintf("%s%s",sh,fh)
+}
+func encrypt(key int, plainText string) string {
+	hashLetters:=hashLetters(key,originalLetters)
+	var hashedString = ""
+
+	findOne := func(r rune)rune{
+		pos:= strings.Index(originalLetters, string([]rune{r}))
+		if pos!=-1 {
+			letterPosition:=(pos+len(originalLetters))%len(originalLetters)
+			hashedString = hashedString+string(hashLetters[letterPosition])
+		}
+		return r
+	}
+
+
+	strings.Map(findOne,plainText)
+	return hashedString
+
+}
+func decrypt(key int, encrypted string) string {
+	
+}
+
+func main()  {
+	plainText:="HELLOWORLD"
+	fmt.Println("Plain Text: ",plainText)
+	encrypted:= encrypt(5,plainText)
+	fmt.Println("Encrypted Text: ",encrypted)
+	decrypted:=decrypt(5,encrypted)
+	fmt.Println("Decrypted Text: ",decrypted)
+}
